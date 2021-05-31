080afbca1918d7b8b6b3b77c8c3afceb478c02fd
ninja076786 <77653028+ninja076786@users.noreply.github.com>
2021-05-30 20:45:24 -0700

    Update mmm.js

--- mmm.js
+++ mmm.js
@@ -1,11 +1,11 @@
-var ee1=localStorage.getItem("zaza1");
-var ee2=localStorage.getItem("zaza2");
-document.getElementById("name_1").innerHTML=ee1+" : ";
-document.getElementById("name_2").innerHTML=ee2+" : ";
+var numn1=localStorage.getItem("numm1");
+var numn2=localStorage.getItem("numm2");
+document.getElementById("name_1").innerHTML=num1+" : ";
+document.getElementById("name_2").innerHTML=num2+" : ";
 function send(){
     var num1=document.getElementById("number_input_1").value;
     var num2=document.getElementById("number_input_2").value;
-    number="<h4>This Guess This Equation That: "+ zaza1 +" Has Wrote! "+"</h4>";
+    number="<h4>This Guess This Equation That: "+ numn1 +" Has Wrote! "+"</h4>";
     nunu="<h4> The Equation Is: "+num1+" x "+num2+" </h4>";
     answer="<br> Type The Answer Here: <input type='text' placeholder='Type Your Answer Here'>";
     button_check="<br><br><button class='btn btn-success' onlick='check_the_answer()'>Check Your Answer</button>";
