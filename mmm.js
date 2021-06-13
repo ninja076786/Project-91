7818255649797f916301d755a7d4362e928b41e2
ninja076786 <77653028+ninja076786@users.noreply.github.com>
2021-06-13 13:41:55 -0700

    Update mmm.js

--- mmm.js
+++ mmm.js
@@ -1,20 +1,25 @@
-var numn1=localStorage.getItem("numm1");
-var numn2=localStorage.getItem("numm2");
-document.getElementById("name_1").innerHTML=num1+" : ";
-document.getElementById("name_2").innerHTML=num2+" : ";
+ numn1=localStorage.getItem("numm1");
+ numn2=localStorage.getItem("numm2");
 function send(){
+    document.getElementById("name_1").innerHTML=numn1+" : ";
+    document.getElementById("name_2").innerHTML=numn2+" : ";
+    document.getElementById("question_turn").innerHTML="Question Turn: "+numn1+" !";
+    document.getElementById("answer_turn").innerHTML="Answer Turn: "+numn2+" !";
+    document.getElementById("num1").value="";
+    document.getElementById("num2").value="";
     var num1=document.getElementById("number_input_1").value;
     var num2=document.getElementById("number_input_2").value;
+    var acal_answ=parseInt(num1)*parseInt(num2);
     number="<h4>This Guess This Equation That: "+ numn1 +" Has Wrote! "+"</h4>";
     nunu="<h4> The Equation Is: "+num1+" x "+num2+" </h4>";
-    answer="<br> Type The Answer Here: <input type='text' placeholder='Type Your Answer Here'>";
-    button_check="<br><br><button class='btn btn-success' onlick='check_the_answer()'>Check Your Answer</button>";
+    answer="<br> Type The Answer Here: <input type='text' id='input_check_box'placeholder='Type Your Answer Here'>";
+    button_check="<br><br><button class='btn btn-success' onlick='check()'>Check Your Answer</button>";
     var the_diver=number+nunu+answer+button_check;
     document.getElementById("output").innerHTML=the_diver;
     document.getElementById("name_1").innerHTML=numn1+" : ";
     document.getElementById("name_2").innerHTML=numn2+" : ";
-    document.getElementById("question_turn").innerHTML=numn1+" : ";
-    document.getElementById("answer_turn").innerHTML=numn2+" : ";
+    document.getElementById("question_turn").innerHTML="Question Turn: "+numn1+" !";
+    document.getElementById("answer_turn").innerHTML="Answer Turn: "+numn2+" !";
     document.getElementById("num1").value="";
     document.getElementById("num2").value="";
-       }
+}
