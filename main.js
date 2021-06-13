090b09df3f96525e05a441706c102a48db71fc5e
ninja076786 <77653028+ninja076786@users.noreply.github.com>
2021-06-13 13:41:35 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-function MUAHAHA(){
+function goto(){
+  var numm1=document.getElementById("number_input_1").value;
+  var numm2=document.getElementById("number_input_2").value;
 var numm1=document.getElementById("player1_name_input_1").value;
 var numm2=document.getElementById("player2_name_input_2").value;
-var acal_answ=parseInt(numm1)*parseInt(numm2);
   localStorage.setItem("numm1",numm1);
   localStorage.setItem("numm2",numm2);
   window.location="moneyy.html";
      }
+     
+
