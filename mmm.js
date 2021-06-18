b97b0ebe9f5fae70738f28e277e34e9d88a6e824
ninja076786 <77653028+ninja076786@users.noreply.github.com>
2021-06-17 17:43:02 -0700

    Update mmm.js

--- mmm.js
+++ mmm.js
@@ -9,7 +9,6 @@ function send(){
     document.getElementById("num2").value="";
     var num1=document.getElementById("number_input_1").value;
     var num2=document.getElementById("number_input_2").value;
-    var acal_answ=parseInt(num1)*parseInt(num2);
     number="<h4>This Guess This Equation That: "+ numn1 +" Has Wrote! "+"</h4>";
     nunu="<h4> The Equation Is: "+num1+" x "+num2+" </h4>";
     answer="<br> Type The Answer Here: <input type='text' id='input_check_box'placeholder='Type Your Answer Here'>";
@@ -22,4 +21,29 @@ function send(){
     document.getElementById("answer_turn").innerHTML="Answer Turn: "+numn2+" !";
     document.getElementById("num1").value="";
     document.getElementById("num2").value="";
+    
+}
+question_player="player_1";
+answer_player="player_2";
+function check(){
+    var num1=document.getElementById("number_input_1").value;
+    var num2=document.getElementById("number_input_2").value;
+    var acal_answ=parseInt(num1)*parseInt(num2);
+    get_answer=document.getElementById("input_check_box").value
+    if(get_answer==acal_answ){
+updateplayer1score=player_1_score+1
+document.getElementById("player_1_score").innerHTML=updateplayer1score
+    }
+    else{
+        updateplayer2score=player_2_score+1
+        document.getElementById("player_2_score").innerHTML=updateplayer2score  
+    }
+    if(question_player=="player1"){
+        question_player="player_2"
+        document.getElementById("question_turn").innerHTML=numn2
+    }
+    else{
+        question_player="player_1"
+        document.getElementById("question_turn").innerHTML=numn1 
+    }
 }
