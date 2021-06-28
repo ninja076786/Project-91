06664dbe9d343e52f54bcf39a9f8d4ef66e2c224
ninja076786 <77653028+ninja076786@users.noreply.github.com>
2021-06-28 16:26:31 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 function goto(){
-  var numm1=document.getElementById("number_input_1").value;
-  var numm2=document.getElementById("number_input_2").value;
 var numm1=document.getElementById("player1_name_input_1").value;
 var numm2=document.getElementById("player2_name_input_2").value;
   localStorage.setItem("numm1",numm1);
