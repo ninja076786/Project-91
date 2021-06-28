30758d3a20fe6a108002eccb6f68fad0cd230757
ninja076786 <77653028+ninja076786@users.noreply.github.com>
2021-06-28 16:29:24 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 function goto(){
-var numm1=document.getElementById("player1_name_input_1").value;
-var numm2=document.getElementById("player2_name_input_2").value;
-  localStorage.setItem("numm1",numm1);
-  localStorage.setItem("numm2",numm2);
+var nummo1=document.getElementById("player1_name_input_1").value;
+var nummo2=document.getElementById("player2_name_input_2").value;
+  localStorage.setItem("nummo1",nummo1);
+  localStorage.setItem("nummo2",nummo2);
   window.location="moneyy.html";
-     }
-     
-
+}
